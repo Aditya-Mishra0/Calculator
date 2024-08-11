d370c9ca505cd952204d90515e3cdf3a62007557
Aditya-Mishra0 <Adityaa.mishra1012@gmail.com>
2024-08-11 23:21:56 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,12 @@
+console.log("hello")
+const button1 = document.querySelector(".button");
+const Operation1 = document.querySelector("Operation");
+const Result1 = document.querySelector("#Result");
+let num1 ;
+let num2 ;
+let num3 ;
+function addition(){
+    num3 = num1 + num2 + num3;
+    return num3;
+}
+console.log(addition());
