1a76421d1e73dd74627e51f8f753313f92453a85
Aditya-Mishra0 <Adityaa.mishra1012@gmail.com>
2024-08-15 22:25:48 +0530

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,16 +1,41 @@
 console.log("hello")
-const button1 = document.querySelector(".button");
-const Operation1 = document.querySelector(".Operation");
-const Result1 = document.querySelector("#Result");
-let num1 ;
-let num2 ;
+document.addEventListener('DOMContentLoaded', () => {
+    const button1 = document.querySelectorAll('.button');
+    const Operation1 = document.querySelectorAll('.Operation')
+    const resultDisplay = document.querySelector("#Result")
+    const Equals = document.querySelector("#equate")
+    const clears = document.querySelector("#All clear")
+
+let num1 = null ;
+let num2 = null ;
 let num3 = 0;
-function addition(){
-    num3 = num1 + num2 + num3;
-    return num3;
-}
-console.log(addition());
-function subtraction(){
-    num3 = num1 - num2 + num3;
-    return num3;
-}
+let currentOperation = null;
+
+Equals.addEventListener('click', () => {
+    num2 = parseFloat(num3);
+    if (num1 != null && num2 != null ){
+        switch(currentOperation){
+            case "add":
+                resultDisplay.textContent = num1+num2;
+                break;
+                case "subtract":
+                    resultDisplay.textContent = num1-num2;
+                    break;
+                    case "multiply":
+                        resultDisplay.textContent = num1*num2;
+                        break;
+                        case "divide":
+                            if(num2 != 0){
+                            resultDisplay.textContent = num1/num2;
+                            }
+                            else{
+                                resultDisplay.textContent = error;
+                            }
+                            break;
+                            default:
+                                resultDisplay.textContent = "error";
+        }
+    }
+});
+});
+
